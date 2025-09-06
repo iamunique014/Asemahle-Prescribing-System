a16b2f2cde37622e396bd19e1584489b107f2cf4
iamunique014 <alungileunique@gmail.com>
2025-09-07 01:23:43 +0200

    Add: Prescription and Medication Models

---
+++ Models/MedicationItem.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PrescribingSystem.Models
+{
+    public class MedicationItem
+    {
+        [Key]
+        public int MedicationItemId { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Prescription))]
+        public int PrescriptionId { get; set; }
+        public Prescription Prescription { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Medication))]
+        public int MedicationId { get; set; }
+        public Medication Medication { get; set; }
+
+        // Prescription-specific fields
+        public string Dosage { get; set; }  // Can override catalog dosage
+        public int Quantity { get; set; }
+        public decimal Price { get; set; } // Price at time of prescription
+    }
+}
---
+++ Models/Prescription.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PrescribingSystem.Models
+{
+    public class Prescription
+    {
+        [Key]
+        public int PrescriptionId { get; set; }
+
+        [Required]
+        public string CustomerId { get; set; }  // Assuming Identity User Id
+
+        [Required]
+        public string DoctorName { get; set; }
+
+        public DateTime DateIssued { get; set; }
+
+        public int TotalRepeats { get; set; }
+
+        public int RemainingRepeats { get; set; }
+
+        public decimal TotalCost { get; set; }
+
+        public string FilePath { get; set; }
+
+        public string PrescriptionStatus { get; set; }
+
+        // Navigation property
+        public ICollection<MedicationItem> MedicationItems { get; set; } = new List<MedicationItem>();
+    }
+}
