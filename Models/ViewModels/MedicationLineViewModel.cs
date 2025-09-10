27c2820ba405b1a589154644cace3ce05ebc05be
iamunique014 <alungileunique@gmail.com>
2025-09-10 17:36:04 +0200

    Add:PrescriptionUpload View Models

---
+++ Models/ViewModels/MedicationLineViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PrescribingSystem.Models.ViewModels
+{
+    public class MedicationLineViewModel
+    {
+        [Required]
+        [Display(Name = "Medication")]
+        public int MedicationId{ get; set; }
+
+        [Required]
+        public string Dosage { get; set; }
+
+        [Required]
+        [Range(1, 1000)]
+        public int Quantity { get; set; }
+
+        [Required]
+        [Range(0.01, 10000)]
+        public decimal Price { get; set; }
+    }
+}
---
+++ Models/ViewModels/PrescriptionUploadViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PrescribingSystem.Models.ViewModels
+{
+    public class PrescriptionUploadViewModel
+    {
+        [Required]
+        [Display(Name = "Doctor Name")]
+        public string DoctorName { get; set; }
+
+        [Required]
+        [Range(1, 10)]
+        public int TotalRepeats { get; set; } = 1;
+
+        [Required]
+        public IFormFile PrescriptionFile { get; set; }
+
+        public List<MedicationLineViewModel> Medications { get; set; } = new List<MedicationLineViewModel>();
+    }
+}
