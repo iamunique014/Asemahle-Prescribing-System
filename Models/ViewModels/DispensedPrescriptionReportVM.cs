2cd74592847b6e738dfe0b0019557f93cf1595b0
iamunique014 <alungileunique@gmail.com>
2025-09-14 02:44:05 +0200

    Add: DispensedPrescriptionReportVM

---
+++ Models/ViewModels/DispensedPrescriptionReportVM.cs
@@ -0,0 +1,13 @@
+﻿namespace PrescribingSystem.Models.ViewModels
+{
+    public class DispensedPrescriptionReportVM
+    {
+        public string PatientName { get; set; }
+        public string DoctorName { get; set; }
+        public string MedicationName { get; set; }
+        public int Quantity { get; set; }
+        public decimal TotalCost { get; set; }
+        public DateTime PrescriptionDate { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+}
