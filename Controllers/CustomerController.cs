76ba6a634f0e1672da91d49d856de4cfdc2ec8ae
iamunique014 <alungileunique@gmail.com>
2025-09-24 19:54:25 +0200

    feat: AddCustomerAllergies Action

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -1,9 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.EntityFrameworkCore;
+using PrescribingSystem.Data;
+using PrescribingSystem.Models;
+using PrescribingSystem.Models.ViewModels;
 
 namespace PrescribingSystem.Controllers
 {
     public class CustomerController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public CustomerController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult CustomerCare()
         {
             return View();
@@ -11,6 +22,20 @@ namespace PrescribingSystem.Controllers
         public IActionResult Reports()
         {
             return View();
+        }
+        [HttpGet]
+        public IActionResult AddCustomerAllergies()
+        {
+
+            var activeIngrediets = _context.ActiveIngredients
+                .Select(ai => new ActiveIngredientViewModel
+                {
+                    id = ai.ActiveIngredientId,
+                    Name = ai.ActiveIngredientName
+
+                }).ToList();
+
+            return View(activeIngrediets);      
         }
     }
 }
