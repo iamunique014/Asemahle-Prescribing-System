4c62986513a89e491c5d83d608c28e8d8d1f563b
iamunique014 <alungileunique@gmail.com>
2025-09-24 19:56:09 +0200

    Add: ActiveIngredientViewModel properties

--- Models/ViewModels/ActiveIngredientViewModel.cs
+++ Models/ViewModels/ActiveIngredientViewModel.cs
@@ -2,6 +2,9 @@
 {
     public class ActiveIngredientViewModel
     {
+        public int id { get; set; }
+        public string Name { get; set; }
+
         public ActiveIngredients NewActiveIngredient { get; set; } = new ActiveIngredients();
         public List<ActiveIngredients> ActiveIngredientsList { get; set; } = new List<ActiveIngredients>();
     }
