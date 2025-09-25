abbc64de6cd03e604365039bb44a7c66f5ef59fd
iamunique014 <alungileunique@gmail.com>
2025-09-26 00:44:28 +0200

    Add: AddCustomerAllergiesViewModel

---
+++ Models/ViewModels/AddCustomerAllergiesViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PrescribingSystem.Models.ViewModels
+{
+    public class AddCustomerAllergiesViewModel
+    {
+        public string UserId { get; set; }
+        public int[] SelectedAllergies { get; set; } = Array.Empty<int>();
+        public List<ActiveIngredientViewModel> ActiveIngredients { get; set; } = new();
+    }
+}
