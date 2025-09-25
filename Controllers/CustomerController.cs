e05e4010c19e899da2927d29cbbfd7ce93af4940
iamunique014 <alungileunique@gmail.com>
2025-09-26 00:43:15 +0200

    fix: AddCustomerAllergies

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -1,18 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.EntityFrameworkCore;
 using PrescribingSystem.Data;
 using PrescribingSystem.Models;
 using PrescribingSystem.Models.ViewModels;
+using System.Security.Claims;
 
 namespace PrescribingSystem.Controllers
 {
     public class CustomerController : Controller
     {
         private readonly ApplicationDbContext _context;
-        public CustomerController(ApplicationDbContext context)
+        private readonly UserManager<ApplicationUser> _userManager;
+        public CustomerController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         public IActionResult CustomerCare()
@@ -24,18 +28,39 @@ namespace PrescribingSystem.Controllers
             return View();
         }
         [HttpGet]
-        public IActionResult AddCustomerAllergies()
+        public IActionResult AddCustomerAllergies(string id)
         {
+            var viewmodel = new AddCustomerAllergiesViewModel
+            {
+                UserId = id,
+                ActiveIngredients = _context.ActiveIngredients
+                    .Select(ai => new ActiveIngredientViewModel
+                    {
+                        id = ai.ActiveIngredientId,
+                        Name = ai.ActiveIngredientName
+                    }).ToList()
+            };
 
-            var activeIngrediets = _context.ActiveIngredients
-                .Select(ai => new ActiveIngredientViewModel
+            return View(viewmodel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddCustomerAllergies(AddCustomerAllergiesViewModel model)
+        {
+            var user = await _userManager.FindByIdAsync(model.UserId);
+            if (user == null) return NotFound("Customer not found");
+
+            foreach (var allergyId in model.SelectedAllergies)
+            {
+                _context.UserAllergies.Add(new UserAllergy
                 {
-                    id = ai.ActiveIngredientId,
-                    Name = ai.ActiveIngredientName
+                    UserId = model.UserId,
+                    ActiveIngredientId = allergyId
+                });
+            }
 
-                }).ToList();
-
-            return View(activeIngrediets);      
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Landing", "Landing"); // or customer dashboard
         }
     }
 }
