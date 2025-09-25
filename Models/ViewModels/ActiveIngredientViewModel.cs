9303010184b80f1810cdd2c81812e94a4ddcb1ef
iamunique014 <alungileunique@gmail.com>
2025-09-26 00:43:56 +0200

    Add: SelectedAllergies Array

--- Models/ViewModels/ActiveIngredientViewModel.cs
+++ Models/ViewModels/ActiveIngredientViewModel.cs
@@ -5,6 +5,7 @@
         public int id { get; set; }
         public string Name { get; set; }
 
+        public int[] SelectedAllergies { get; set; }
         public ActiveIngredients NewActiveIngredient { get; set; } = new ActiveIngredients();
         public List<ActiveIngredients> ActiveIngredientsList { get; set; } = new List<ActiveIngredients>();
     }
