15cbeb05f4244daa34e98ccbb903497fc5c2c1ab
iamunique014 <alungileunique@gmail.com>
2025-10-08 22:16:04 +0200

    Add: OrderItems

---
+++ Models/OrderItem.cs
@@ -0,0 +1,17 @@
+﻿namespace PrescribingSystem.Models
+{
+    public class OrderItem
+    {
+        public int OrderItemId { get; set; }
+
+        public int OrderId { get; set; }
+        public PrescriptionOrders PrescriptionOrders { get; set; }
+
+        public int MedicationItemId { get; set; }
+        public MedicationItem MedicationItem { get; set; }
+
+        public int QuantityOrdered { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal LineTotal { get; set; }
+    }
+}
