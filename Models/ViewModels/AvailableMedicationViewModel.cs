28a3720842242df857640b224aee114c159e53a3
iamunique014 <alungileunique@gmail.com>
2025-10-08 22:16:35 +0200

    Add: ViewModel

---
+++ Models/ViewModels/AvailableMedicationViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace PrescribingSystem.Models.ViewModels
+{
+    public class AvailableMedicationViewModel
+    {
+        public int MedicationItemId { get; set; }
+        public string MedicationName { get; set; }
+        public string DoctorName { get; set; }
+        public int Quantity { get; set; }
+        public int RemainingRepeats { get; set; }
+        public int PrescriptionId { get; set; }
+        public bool Selected { get; set; } // for checkboxes
+    }
+}
