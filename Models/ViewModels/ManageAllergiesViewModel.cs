f0c31e5c50321dfe84bde7d0c5745bf548b991c2
iamunique014 <alungileunique@gmail.com>
2025-10-16 05:43:08 +0200

    Add: ManageAllergiesViewModel

---
+++ Models/ViewModels/ManageAllergiesViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace PrescribingSystem.Models.ViewModels
+{
+    public class ManageAllergiesViewModel
+    {
+        public List<int> SelectedAllergyIds { get; set; } = new();
+        public List<SelectListItem> AvailableAllergies { get; set; } = new();
+    }
+}
