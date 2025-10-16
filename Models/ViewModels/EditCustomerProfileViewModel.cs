76c330cb8c329c7c94ee48a6f5fdafa0b0c2dc83
iamunique014 <alungileunique@gmail.com>
2025-10-16 05:24:40 +0200

    Add: EditCustomerViewModel

---
+++ Models/ViewModels/EditCustomerProfileViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PrescribingSystem.Models.ViewModels
+{
+    public class EditCustomerProfileViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [Required]
+        [Phone]
+        [Display(Name = "Cellphone Number")]
+        [RegularExpression(@"^(?:\+27|0)[6-8][0-9]{8}$", ErrorMessage = "Please enter a valid South African phone number.")]
+        public string CellphoneNumber { get; set; }
+        public string Email { get; set; } 
+    }
+}
