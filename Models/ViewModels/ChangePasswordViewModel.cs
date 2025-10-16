1919aa2f9dbebc41fcba3b98a6fe351ee518f6ed
iamunique014 <alungileunique@gmail.com>
2025-10-16 06:24:44 +0200

    Add: ChangePasswordViewModel

---
+++ Models/ViewModels/ChangePasswordViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PrescribingSystem.Models.ViewModels
+{
+    public class ChangePasswordViewModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Current Password")]
+        public string OldPassword { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "New Password")]
+        public string NewPassword { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("NewPassword", ErrorMessage = "Passwords do not match.")]
+        [Display(Name = "Confirm New Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
